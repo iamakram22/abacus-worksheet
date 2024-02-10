41d2c51bb3cca9a8191b418286e8d64e82ac7e39
Mohd Akram <iamakram22@gmail.com>
2024-02-10 15:02:33 +0530

    minify js & css

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
     <script src="assets/bootstrap.min.js"></script>
     <script src="assets/jquery.min.js"></script>
     <script src="assets/jquery.validate.min.js"></script>
-    <script src="assets/script.js"></script>
+    <script src="assets/script.min.js"></script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,6 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Abacus Worksheet Generator</title>
     <link rel="stylesheet" href="assets/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/style.min.css">
 </head>
 <body>
